658823c2c901da10268398729a4d312ed00e9bbe
Anmol Mahanty <mahantyanmol14@gmail.com>
2026-03-24 22:40:54 +0530

    more pattern practice

---
+++ PatternPrinting/StarTriangleHorizontallyFlipped.java
@@ -0,0 +1,15 @@
+package PatternPrinting;
+import java.util.*;
+public class StarTriangleHorizontallyFlipped {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows :");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
